db1f7c8d48caccd0eefc30e8093d62caab8c6650
Rivio98 <rivaldogjoni1998@gmail.com>
2024-07-31 16:55:50 +0200

    [FIX] Fix content of main added in header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,6 +4,20 @@ export default {
 </script>
 <template>
     <div>
+        <div class="header">
+            <div class="content d-flex p-3">
+                <img src="../assets/1200px-Yu-Gi-Oh!_(Logo).jpg" class="logo">
+                <h1 class="ps-3">YU-GI-OH Api</h1>
+            </div>
+        </div>
     </div>
 </template>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.header {
+    height: 100px;
+
+    .logo {
+        width: 150px;
+    }
+}
+</style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -4,19 +4,6 @@ export default {
 }
 </script>
 <template>
-    <div class="header">
-        <div class="content d-flex p-3">
-            <img src="../assets/1200px-Yu-Gi-Oh!_(Logo).jpg" class="logo">
-            <h1 class="ps-3">YU-GI-OH Api</h1>
-        </div>
-    </div>
-</template>
-<style lang="scss" scoped>
-.header {
-    height: 100px;
 
-    .logo {
-        width: 150px;
-    }
-}
-</style>
+</template>
+<style lang="scss" scoped></style>
